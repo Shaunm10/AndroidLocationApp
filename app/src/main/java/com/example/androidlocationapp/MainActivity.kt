40e466620f139bd3fa103f8b5320e59cb3e96777
shaunsargent <ShaunM10@gmail.com>
2024-04-27 14:42:44 -0400

    working on asking for permissions for Location

--- app/src/main/java/com/example/androidlocationapp/MainActivity.kt
+++ app/src/main/java/com/example/androidlocationapp/MainActivity.kt
@@ -1,13 +1,21 @@
 package com.example.androidlocationapp
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import android.Manifest
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.androidlocationapp.ui.theme.AndroidLocationAppTheme
@@ -22,25 +30,54 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    // Greeting("Android")
                 }
             }
         }
     }
 }
 
+//@Preview(showBackground = true)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun LocationDisplay(
+    locationUtils: LocationUtils,
+    context: Context
+) {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AndroidLocationAppTheme {
-        Greeting("Android")
+    // request permission from the system
+    val requestPermissionLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestMultiplePermissions(),
+        // a callback on the result of the request.
+        onResult = { permissions ->
+
+            // if we did obtain the permission for the
+            if (permissions[Manifest.permission.ACCESS_COARSE_LOCATION] == true &&
+                permissions[Manifest.permission.ACCESS_FINE_LOCATION] == true
+            ) {
+                // permission is granted.
+
+            } else {
+                // ask for permission
+            }
+        })
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = "Location not available")
+        Button(onClick = {
+
+            if (locationUtils.hasLocationPermission(context)) {
+                // permission granted
+            } else {
+                // request location permission
+            }
+        }) {
+            Text(text = "Get Location")
+
+        }
+
     }
+
 }
